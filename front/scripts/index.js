d5f9133b81c6f2bc1ca4b9d5f40e7a5969f97933
alejandro0419 <alejandrocampaya@hotmail.com>
2024-03-04 21:21:12 -0300

    Homework 01

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,37 @@
 console.log(tempData);
+
+function objToCard(objPelicula){
+    const {title, year, director, duration, rate, poster} = objPelicula
+
+    const imagen = document.createElement('img')
+    const titulo = document.createElement('h2')
+    const puntuacion = document.createElement('p')
+    const boton = document.createElement('button')
+
+    imagen.src = poster;
+    titulo.textContent = title;
+    puntuacion.textContent = rate;
+    boton.textContent ='Ver pelicula';
+
+    imagen.classList.add('movieImg');
+    titulo.classList.add('movieTitle');
+    puntuacion.classList.add('movieDesc');
+    boton.classList.add('movieBtn');
+    
+    const divCard = document.createElement('div');
+    divCard.appendChild(imagen);
+    divCard.appendChild(titulo);
+    divCard.appendChild(puntuacion);
+    divCard.appendChild(boton)
+    divCard.classList.add('card')
+
+    return divCard;
+}
+
+const contenedor = document.getElementById('dealer1')
+const listaDeObj = tempData;
+const listaCards = listaDeObj.map(objToCard);
+
+listaCards.forEach(card =>{
+    contenedor.appendChild(card);
+})
