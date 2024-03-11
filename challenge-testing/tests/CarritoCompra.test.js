7b8edbc8a54962e9a4db936fbd99a421d9ec2ff1
alejandro0419 <alejandrocampaya@hotmail.com>
2024-03-10 22:09:45 -0300

    Progreso HW testing avanzado

--- challenge-testing/tests/CarritoCompra.test.js
+++ challenge-testing/tests/CarritoCompra.test.js
@@ -19,12 +19,8 @@ describe('Este test debe pasar siempre',() => {
     })
 })
 */
+//import {expect, jest, test} from '@jest/globals';
 const CarritoCompra = require("./index");
-//const jestMock = require('jest-mock'); // Declaro una variable / funcion mock sin target (?)
-
-//const spy = jest.spyOn(CarritoCompra.prototype, 'constructor');
-
-//const mock = jest.fn();
 const mockCarroCompra = jest.fn(CarritoCompra); //hago mock del constructor para ver cuantas veces es llamado
 
 describe('La clase "CarritoCompra" debe: ', () => {
@@ -34,7 +30,7 @@ describe('La clase "CarritoCompra" debe: ', () => {
     expect(typeof mockCarroCompra.prototype.constructor).toBe("function");
   });
 
-  it('Tener el modulo "constructor" que inicializa el carrito como un array vacío.', () => {
+  it('Tener el metodo "constructor" que inicializa el carrito como un array vacío.', () => {
     const carrito = new CarritoCompra();
     expect(carrito).toBeInstanceOf(CarritoCompra);
     expect(carrito.productos).toEqual([]);
@@ -44,15 +40,15 @@ describe('La clase "CarritoCompra" debe: ', () => {
   //expect(mockConstructor).toHaveBeenCalled();
 
   it('Debe tener el metodo "agregarProducto"', () => {
-    expect(typeof mockCarroCompra.prototype.agregarProducto).toBe("function");
+    expect(typeof CarritoCompra.prototype.agregarProducto).toBe("function");
   });
 
   it('Debe tener el metodo "calcularTotal"', () => {
-    expect(typeof mockCarroCompra.prototype.calcularTotal).toBe("function");
+    expect(typeof CarritoCompra.prototype.calcularTotal).toBe("function");
   });
 
   it('Debe tener el metodo "aplicarDescuento"', () => {
-    expect(typeof mockCarroCompra.prototype.aplicarDescuento).toBe("function");
+    expect(typeof CarritoCompra.prototype.aplicarDescuento).toBe("function");
   });
 
   it('El metodo "agregarProducto" recibe un objeto representando un producto y lo agrega al carrito', () => {
@@ -65,20 +61,20 @@ describe('La clase "CarritoCompra" debe: ', () => {
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Lechuga", precio: 10, cantidad: 2 })
     carrito.agregarProducto({ nombre: "Tomate", precio: 20, cantidad: 1 })
-    expect(carrito.calcularTotal()).toHaveReturnedWith(40);
+    expect(carrito.calcularTotal()).toBe(40);
   })
   it('El metodo "calcularTotal" Calcula el total de la compra sumando los precios de todos los productos en el carrito', ()=>{
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Sandia", precio: 30, cantidad: 1 })
     carrito.agregarProducto({ nombre: "Banana", precio: 5, cantidad: 7 })
-    expect(carrito.calcularTotal()).toHaveReturnedWith(65);
+    expect(carrito.calcularTotal()).toBe(65);
   })
   it('El metodo "calcularTotal" Calcula el total de la compra sumando los precios de todos los productos en el carrito', ()=>{
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Milanesa", precio: 10, cantidad: 5 })
     carrito.agregarProducto({ nombre: "Zapallo", precio: 20, cantidad: 1 })
     
-    expect(carrito.calcularTotal()).toHaveReturnedWith(70);
+    expect(carrito.calcularTotal()).toBe(70);
   })
   
   
@@ -86,21 +82,21 @@ describe('La clase "CarritoCompra" debe: ', () => {
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Milanesa", precio: 10, cantidad: 5 })
     carrito.agregarProducto({ nombre: "Zapallo", precio: 20, cantidad: 1 })
-    expect(carrito.aplicarDescuento(50)).toHaveReturnedWith(35);
+    expect(carrito.aplicarDescuento(50)).toBe(35);
   })
   
   it('El metodo "aplicarDescuento" Aplica un descuento al total de la compra según el porcentaje especificado.', ()=>{
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Milanesa", precio: 10, cantidad: 5 })
     carrito.agregarProducto({ nombre: "Zapallo", precio: 20, cantidad: 1 })
-    expect(carrito.aplicarDescuento(100)).toHaveReturnedWith(0);
+    expect(carrito.aplicarDescuento(100)).toBe(0);
   })
   
   it('El metodo "aplicarDescuento" Aplica un descuento al total de la compra según el porcentaje especificado.', ()=>{
     const carrito = new CarritoCompra();
     carrito.agregarProducto({ nombre: "Milanesa", precio: 10, cantidad: 5 })
     carrito.agregarProducto({ nombre: "Zapallo", precio: 20, cantidad: 1 })
-    expect(carrito.aplicarDescuento(0)).toHaveReturnedWith(70);
+    expect(carrito.aplicarDescuento(0)).toBe(70);
   })
 
   
--- challenge-testing/tests/index.js
+++ challenge-testing/tests/index.js
@@ -1,5 +1,30 @@
-class CarritoCompra{
-    constructor(){    this.productos = [];}
+class CarritoCompra {
+  constructor() {
+    this.productos = [];
+  }
+  agregarProducto(objProducto){
+    this.productos.push(objProducto)
+  }
+  calcularTotal(){
+    let total = 0;
+    this.productos.forEach(objProducto => {
+        total += objProducto.precio * objProducto.cantidad;
+    });
+    return total;
+}
+aplicarDescuento(descuento){
+    if(descuento > 100 || descuento < 0) return "Ingresar un valor del 1 al 100";
+    if(descuento === 100) return 0;
+    
+    let total=0;
+    this.productos.forEach(objProducto => {
+        total += objProducto.precio * objProducto.cantidad;
+    });
+
+    if(descuento === 0) return total;
+    let monto = (total - (descuento/100))
+    return monto;
+  }
 }
 
 module.exports = CarritoCompra;
