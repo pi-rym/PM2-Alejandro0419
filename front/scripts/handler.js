40c273386e72380851b946a95398140f00634565
alejandro0419 <alejandrocampaya@hotmail.com>
2024-03-20 00:15:23 -0300

    Homework 12 M2 Completada

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -13,11 +13,27 @@ async function getMovies() {
 
 async function postMovie(event) {
   event.preventDefault();
+  
+  const formCreate = document.getElementById('submitMovie');
+  const dataForm = new FormData(formCreate);
+  console.log(dataForm);
+  
+  const movieData = {
+    title: dataForm.get('title'),
+    year: dataForm.get('year'),
+    director: dataForm.get('director'),
+    duration: dataForm.get('duration'),
+    genre: dataForm.getAll('genre'),
+    rate: dataForm.get('rate'),
+    poster: dataForm.get('poster'),
+  };
+
   try {
-    const formCreate = document.getElementById('submitMovie');
-    const formData = new FormData(formCreate);
-    const data = await axios.post('http://localhost:3000/movies', formData);
+   /*  const response = await axios.post('http://localhost:3000/movies', new URLSearchParams(new FormData(document.getElementById('submitMovie'))));
+    console.log(response.data); */
+    const data = await axios.post('http://localhost:3000/movies', movieData);
     console.log("Data del form enviada: " + data);
+
   } catch (error) {
     console.log("Ha ocurrido un error en la conexion al servidor. - postMovie")
     console.log(error.message)
